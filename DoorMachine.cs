9496e38320f538eca43a37cde33f3c5e09228447
MuhammadAzka123 <azkalubis@gmail.com>
2026-03-15 21:03:03 +0700

    menambahkan class DoorMachine

---
+++ DoorMachine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_MODUL4_103022400048
+{
+    public class DoorMachine
+    {
+        enum State
+        {
+            Terkunci,
+            Terbuka
+        }
+        private State state;
+        public DoorMachine()
+        {
+            state = State.Terkunci;
+            Console.WriteLine("Pintu terkunci");
+        }
+        public void BukaPintu()
+        {
+            if (state == State.Terkunci)
+            {
+                state = State.Terbuka;
+                Console.WriteLine("Pintu tidak terkunci");
+            }
+        }
+        public void KunciPintu()
+        {
+            if (state == State.Terbuka)
+            {
+                state = State.Terkunci;
+                Console.WriteLine("Pintu terkunci");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using TP_MODUL4_103022400048;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //kodepos
+        KodePos kode = new KodePos();
+
+        Console.Write("Masukkan kelurahan: ");
+        string kel = Console.ReadLine();
+
+        int hasil = kode.getKodePos(kel);
+
+        if (hasil != -1)
+        {
+            Console.WriteLine("Kode Pos : " + hasil);
+        }
+        else
+        {
+            Console.WriteLine("Kelurahan tidak ditemukan");
+        }
+        Console.WriteLine("");
+
+        //doormachine
+        DoorMachine pintu = new DoorMachine();
+
+        pintu.BukaPintu();
+        pintu.KunciPintu();
+    }
+}
